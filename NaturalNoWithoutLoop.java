d49da0e36660b24d0010f9e3040884645e9d48cb
Santosh Gupta <73736582+Santosh5358@users.noreply.github.com>
2023-02-07 08:33:58 +0000

    Reduce space time

--- NaturalNoWithoutLoop.java
+++ NaturalNoWithoutLoop.java
@@ -1,16 +1,25 @@
 import java.util.*;
 public class NaturalNoWithoutLoop {
-    static void NaturalNo(int a,int b){
-        if(a==b){
-            System.out.print(b);
+    // static void NaturalNo(int a,int b){
+    //     if(a==b){
+    //         System.out.print(b);
+    //     }else{
+    //         System.out.print(a+" ");
+    //         NaturalNo(a+1, b);
+    //     }
+    // }
+    static void NaturalNo(int a){
+        if(a<=0){
+           return;
         }else{
+            
+            NaturalNo(a-1);
             System.out.print(a+" ");
-            NaturalNo(a+1, b);
         }
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
-        NaturalNo(1,a);
+        NaturalNo(a);
     }
 }
