1278306a9e2c8771f9bf8aa7a7c4347d37dce060
Santosh Gupta <73736582+Santosh5358@users.noreply.github.com>
2023-02-25 15:11:08 +0000

    String validate

---
+++ stackQuestion.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class stackQuestion {
+    public static void main(String[] args) {
+        String s="(){}[]"; // true
+        // String s="({[})";   // false
+        System.out.println(VaildString(s));
+    }
+
+    static boolean VaildString(String s) {
+        Stack <Character> stack=new Stack<>();
+        char []c=s.toCharArray();
+        for(char i:c){
+            if(i=='}'){
+                if(stack.peek()=='{'){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }else if(i==']'){
+                if(stack.peek()=='['){
+                    stack.pop();
+                }else
+                    return false;
+
+            }else if(i==')'){
+                if(stack.peek()=='('){
+                    stack.pop();
+                }else
+                    return false;
+            }else{
+                stack.push(i);
+            }
+        }
+        if(stack.size()==0)
+                return true;
+            else
+                return false;
+    }
+}
